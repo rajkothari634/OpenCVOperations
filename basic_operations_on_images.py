c2801abed66ff8228fb12cedceb5beee5a79a59c
Raj kothari <rkmeansrk39@gmail.com>
2020-03-19 22:57:47 +0530

    operation on image

---
+++ basic_operations_on_images.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+img = cv2.imread('messi5.jpg')
+img2 = cv2.imread('opencv-logo.png')
+print(img.shape)
+print(img.size)
+print(img.dtype)
+b,g,r = cv2.split(img)
+img = cv2.merge((b,g,r))
+
+ball = img[280:340,330:390]
+cv2.imshow("img",ball)
+status = cv2.imwrite("ball.jpg",ball)
+print("working",status)
+img[273:333,100:160] = ball
+img = cv2.resize(img,(512,512))
+img2 = cv2.resize(img2,(512,512))
+#dst = cv2.add(img,img2)
+dst = cv2.addWeighted(img,0.2,img2,0.8,0)
+cv2.imshow('image',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
