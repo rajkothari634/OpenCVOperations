2c1e253a6c60566114e60cd198ca48b2858fd108
Raj kothari <rkmeansrk39@gmail.com>
2020-04-14 16:02:46 +0530

    update

--- basic_motion_detection_opencv_python.py
+++ basic_motion_detection_opencv_python.py
@@ -10,9 +10,8 @@ while cap.isOpened():
     gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray,(5,5),0)
     _,thresh = cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh,None,iterations=3)
-    contours,_ = cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
+    dilated = cv2.dilate(thresh,None,iterations=2)
+    _,contours,_ = cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     #cv2.drawContours(frame1,contours,-1,(0,255,0),2)
     for contour in contours:
         (x,y,w,h) = cv2.boundingRect(contour)
